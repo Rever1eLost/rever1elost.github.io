f0eda0c6f0da3586ca6fc283511b7acc6dc07e34
chuma boi <smnsokolov@gmail.com>
2024-05-13 22:28:34 +0300

    da

--- src/footer.js
+++ src/footer.js
@@ -10,28 +10,28 @@ function Footer() {
         <div className="footer-column">
           <h4>About</h4>
           <ul>
-            <li><a href="/about">About Us</a></li>
-            <li><a href="/contact">Contact Us</a></li>
-            <li><a href="/terms">Terms of Service</a></li>
-            <li><a href="/privacy">Privacy Policy</a></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
+            <li><Link to="/terms">Terms of Service</Link></li>
+            <li><Link to="/privacy">Privacy Policy</Link></li>
           </ul>
         </div>
         <div className="footer-column">
           <h4>Links</h4>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/news">News</a></li>
-            <li><a href="/matches">Matches</a></li>
-            <li><a href="/results">Results</a></li>
-            <li><a href="/login">Login</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/news">News</Link></li>
+            <li><Link to="/matches">Matches</Link></li>
+            <li><Link to="/results">Results</Link></li>
+            <li><Link to="/login">Login</Link></li>
           </ul>
         </div>
         <div className="footer-column">
           <h4>Follow Us</h4>
           <ul>
-            <li><a href="" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-            <li><a href="" target="_blank" rel="noopener noreferrer">Facebook</a></li>
-            <li><a href="" target="_blank" rel="noopener noreferrer">YouTube</a></li>
+            <li><Link to="" target="_blank" rel="noopener noreferrer">Twitter</Link></li>
+            <li><Link to="" target="_blank" rel="noopener noreferrer">Facebook</Link></li>
+            <li><Link to="" target="_blank" rel="noopener noreferrer">YouTube</Link></li>
           </ul>
         </div>
       </div>
